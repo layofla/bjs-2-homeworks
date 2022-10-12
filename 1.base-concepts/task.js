8e8b342d390f5623c6d945010be229b38f9c57c4
Asya Douglas <asya.douglas17@gmail.com>
2022-10-12 18:12:47 +0800

    change task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,20 @@
+"use strict"
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
+  let arr=[];
+  let x1;
+  let x2;
+  let D=b**2-4*a*c;
+  if (D<0) {
+    arr=[];
+  } else if (D===0) {
+    x1=-b/(2*a);
+    arr.push(x1);
+  } else if (D>0) {
+    x1=-b+Math.sqrt(D)/(2*a);
+    arr.push(x1);
+    x2=-b-Math.sqrt(D)/(2*a);
+    arr.push(x2);
+  }
   return arr; // array
 }
 
