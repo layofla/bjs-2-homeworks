c0cfd3b75353399951b7a2bc44da03bb2e462dc9
Asya Douglas <asya.douglas17@gmail.com>
2022-10-19 14:17:05 +0800

    change task

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -3,19 +3,15 @@ function solveEquation(a, b, c) {
   let arr=[];
   let x1;
   let x2;
-  let D=b**2-4*a*c;
-  if (D<0) {
-    arr=[];
-  } else if (D===0) {
-    x1=-b/(2*a);
-    arr.push(x1);
-  } else if (D>0) {
-    x1=-b+Math.sqrt(D)/(2*a);
-    arr.push(x1);
-    x1=-b-Math.sqrt(D)/(2*a);
-    arr.push(x2);
+  let d=b**2-4*a*c;
+  if (d<0) {
+    arr;
+  } else if (d===0) { 
+    arr.push(x1=-b/(2*a));
+  } else if (d>0) {
+    arr.push((-b+Math.sqrt(d))/(2*a));
+    arr.push((-b-Math.sqrt(d))/(2*a));
   }
-
   return arr; // array
 }
 
