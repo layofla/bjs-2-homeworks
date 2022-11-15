d706156df2f1f206998142c714f65389de7d4efd
Asya Douglas <asya.douglas17@gmail.com>
2022-11-15 20:18:57 +0800

    change

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,13 @@
 function compareArrays(arr1, arr2) {
   let result;
-
-  // Ваш код
+result=arr1.length === arr2.length && arr1.every((a,i)=> a === arr2[i]);
 
   return result; // boolean
 }
 
 function advancedFilter(arr) {
   let resultArr;
-
-  // Ваш код
+resultArr=arr.filter((element) => element > 0).filter((element) => element % 3 === 0).map((element) => element * 10)
 
   return resultArr; // array
 }
