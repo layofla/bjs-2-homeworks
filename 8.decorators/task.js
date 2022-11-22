3162b03295e9bfe198d5eeedb98e1978c5cf16ed
Asya Douglas <asya.douglas17@gmail.com>
2022-11-22 22:33:11 +0800

    change

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,8 +1,49 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+
+  function wrapper(...args) {
+    const hash = args.join(',');
+    let objectInCache = cache.findIndex((item) => item.hash === hash); 
+    if (objectInCache !== -1) { 
+        console.log("Из кэша: " + cache[objectInCache].result); 
+        return "Из кэша: " + cache[objectInCache].result;
+    }
+
+    let result = func(...args); 
+    cache.push({hash, result}) ; 
+    if (cache.length > 5) { 
+      cache.shift(); 
+    }
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;  
 }
+return wrapper;
+}
+
 
 
 function debounceDecoratorNew(func) {
-  // Ваш код
-}
+  let time = null;
+  
+function wrapper (...args){
+    if (time === null) {
+      func(...args);
+    }
+    clearTimeout(time);
+    time = setTimeout(() => time = null, ms);
+  }
+  return wrapper;
+} 
+
+function debounceDecorator(func, ms) {
+  let time = null;
+  function wrapper(...args){
+    if (time === null) {
+      func(...args);
+    }
+    clearTimeout(time);
+    time = setTimeout(() => time = null, ms);
+    wrapper.count++;
+  }
+  return wrapper;
+} 
